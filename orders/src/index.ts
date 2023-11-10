ecfde53c19e3a4a3c0b0e597d46b2d8026799c8d
KhoGusher <kondwaninantchito@gmail.com>
2023-11-10 11:30:16 +0200

    testing orders

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -43,6 +43,10 @@ const start = async () => {
     new PaymentCreatedListener(natsWrapper.client).listen();
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to Mongo Database");
+    console.log("Connected to Mongo Database");
+    console.log("Connected to Mongo Database");
+    console.log("Connected to Mongo Database");
+    console.log("Connected to Mongo Database");
   } catch (err) {
     console.error(err);
   }
